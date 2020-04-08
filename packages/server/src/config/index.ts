b47f14fb821c91503a4aeee1cb4eb6d7a29ce94b
by1773 <1747837358@qq.com>
2020-04-08 13:56:52 +0800

    config

--- packages/server/src/config/index.ts
+++ packages/server/src/config/index.ts
@@ -4,16 +4,16 @@
  * @Author: by1773
  * @Date: 2020-03-31 17:32:55
  * @LastEditors: by1773
- * @LastEditTime: 2020-04-02 17:32:33
+ * @LastEditTime: 2020-04-08 13:56:26
  */
 export const config = {
-  admin: { name: 'admin', password: 'admin' },
+  admin: { name: 'admin', password: 'ting' },
   mysql: {
-    host: 'localhost',  //0.0.0.0:3306
+    host: 'http://139.9.5.251',  //0.0.0.0:3306
     port: 3307,
     username: 'root',
     password: 'root',
-    database: 'wipi',
+    database: 'ting-website',
     charset: 'utf8mb4',
   },
 };
