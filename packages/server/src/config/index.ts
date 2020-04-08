8b5e33d061841164e4576603bb125b0dda2b773d
by1773 <1747837358@qq.com>
2020-04-08 13:57:53 +0800

    config

--- packages/server/src/config/index.ts
+++ packages/server/src/config/index.ts
@@ -4,13 +4,13 @@
  * @Author: by1773
  * @Date: 2020-03-31 17:32:55
  * @LastEditors: by1773
- * @LastEditTime: 2020-04-08 13:56:26
+ * @LastEditTime: 2020-04-08 13:57:46
  */
 export const config = {
   admin: { name: 'admin', password: 'ting' },
   mysql: {
     host: 'http://139.9.5.251',  //0.0.0.0:3306
-    port: 3307,
+    port: 3306,
     username: 'root',
     password: 'root',
     database: 'ting-website',
